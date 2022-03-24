6606c009ad5d092f8144089f36ce2a4c338fce45
fabrv <35388973+fabrv@users.noreply.github.com>
2022-03-24 19:55:44 +0000

    Deploying to gh-pages from @ fabrv/elm-todo@fc188793e6eea7dbe8e6a28878638a17cf5e8863 🚀

---
+++ precache-manifest.07e531096b8e5b25c105679ff15a52fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "7130c4772cbccdd9fcbf7671200ecdce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "4813575a552ae2c7f454",
+    "url": "/static/css/main.5c14bf26.chunk.css"
+  },
+  {
+    "revision": "4813575a552ae2c7f454",
+    "url": "/static/js/main.4813575a.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "354e092f0aa2734f8f4c",
+    "url": "/static/js/vendors~main.354e092f.chunk.js"
+  }
+]);
